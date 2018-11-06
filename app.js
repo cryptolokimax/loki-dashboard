6cf29f1b335a426571ce41970701b24d300a25e1
Maxim Antonov <maximantonov@Maxims-MacBook-Pro.local>
2018-11-07 00:20:22 +0200

    Add app.js to serve static

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var proxy = require('http-proxy-middleware');
+
+var port = process.env.PORT || process.argv[2] || 4000;
+
+var app = express();
+ 
+app.use('/config', proxy({target: 'http://localhost:5000', changeOrigin: true}));
+const wsProxy = proxy('/socket.io', {target:'ws://localhost:5000', changeOrigin: true, ws:true});
+app.use(wsProxy);
+
+app.use('/', express.static('build'));
+app.listen(port, function () {
+    console.log('app up on port: ' + port);
+});
+
+ 
