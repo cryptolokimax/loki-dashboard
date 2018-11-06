ac037108ab7ddab9767af65163d775988a81b53f
Maxim Antonov <maximantonov@Maxims-MacBook-Pro.local>
2018-11-07 01:06:25 +0200

    forbid unknown hosts

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var proxy = require('http-proxy-middleware');
+var hostValidation = require('host-validation')
 
 var port = process.env.PORT || process.argv[2] || 4000;
 
 var app = express();
+
+app.use(hostValidation({ hosts: ['127.0.0.1:'+port,
+                                 'localhost:'+port,
+                                 'lokidashboard', 
+                                 /.*\.lokidashboard\.tk$/] }));
  
 app.use('/config', proxy({target: 'http://localhost:5000', changeOrigin: true}));
 const wsProxy = proxy('/socket.io', {target:'ws://localhost:5000', changeOrigin: true, ws:true});
