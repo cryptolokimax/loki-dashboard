4861a20fedcf4f9d1566b6c21bd3c6cc21292705
Maxim Antonov <helloto@me.com>
2020-05-28 16:07:27 +0300

    Use dashboard number in URL

--- app.js
+++ app.js
@@ -1,23 +1,34 @@
-var express = require('express');
-var proxy = require('http-proxy-middleware');
-var hostValidation = require('host-validation')
+var express = require("express");
+var proxy = require("http-proxy-middleware");
+var hostValidation = require("host-validation");
 
 var port = process.env.PORT || process.argv[2] || 4000;
 
 var app = express();
 
-app.use(hostValidation({ hosts: ['127.0.0.1:'+port,
-                                 'localhost:'+port,
-                                 'lokidashboard.com:'+port,
-                                 /.*\.lokidashboard\.com$/] }));
- 
-app.use('/config', proxy({target: 'http://localhost:5000', changeOrigin: true}));
-const wsProxy = proxy('/socket.io', {target:'ws://localhost:5000', changeOrigin: true, ws:true});
+app.use(
+  hostValidation({
+    hosts: [
+      "127.0.0.1:" + port,
+      "localhost:" + port,
+      "lokidashboard.com:" + port,
+      /.*\.lokidashboard\.com$/,
+    ],
+  })
+);
+
+app.use(
+  "/config",
+  proxy({ target: "http://localhost:5000", changeOrigin: true })
+);
+const wsProxy = proxy("/socket.io", {
+  target: "ws://localhost:5000",
+  changeOrigin: true,
+  ws: true,
+});
 app.use(wsProxy);
 
-app.use('/', express.static('build'));
+app.use(express.static("build"));
 app.listen(port, function () {
-    console.log('app up on port: ' + port);
+  console.log("app up on port: " + port);
 });
-
- 
